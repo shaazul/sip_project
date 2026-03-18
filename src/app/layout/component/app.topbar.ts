64a6d80372c0770012786b4a369ee19421942178
Farisha <farisha1700@gmail.com>
2026-03-18 09:25:45 +0800

    update RDMA logo

--- src/app/layout/component/app.topbar.ts
+++ src/app/layout/component/app.topbar.ts
@@ -41,7 +41,7 @@ import moment from 'moment-timezone';
       </button> -->
       <a class="layout-topbar-logo" routerLink="/package/landing">
         <img src="assets/icons/RDM_Logo.png" alt="RDM"
-             class="layout-topbar-logo-image w-24 sm:w-30 md:w-38 lg:w-44 h-auto" />
+             class="layout-topbar-logo-image w-28 sm:w-36 md:w-44 lg:w-52 h-auto" />
       </a>
     </div>
 
